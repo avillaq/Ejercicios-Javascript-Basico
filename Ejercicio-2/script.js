27242f3baf9243a28ee282f6b285a339931231c3
avillaq <villafuertequispealex@gmail.com>
2024-05-11 13:05:59 -0500

    Crea una funcion para invertir una palabra en el archivo js

---
+++ Ejercicio-2/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',init);
+function init(){
+    function invertirPalabra(palabra) {
+        let palabraInvertida = "";
+        for (let i = palabra.length - 1; i >= 0; i--) {
+            palabraInvertida += palabra[i];
+        }
+        return palabraInvertida;
+    }
+
+
+
+}

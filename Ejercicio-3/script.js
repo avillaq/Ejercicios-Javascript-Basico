4736aaee3058113351bc15e1a38abb08bc889b90
avillaq <villafuertequispealex@gmail.com>
2024-05-11 16:52:52 -0500

    Crea variable para convertir milisegundos a dias

--- Ejercicio-3/script.js
+++ Ejercicio-3/script.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded',init);
 function init(){
     let diaArequipa = new Date();
-    diaArequipa.setFullYear(2024, 8, 15);
+    diaArequipa.setFullYear(2024, 7, 15);
 
     let diaHoy = new Date();
 
+    let minuto = 1000 * 60;
+    let hora = minuto * 60;
+    let dia = hora * 24;
 
 
 }

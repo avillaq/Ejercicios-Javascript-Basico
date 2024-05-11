2752ae16e2ce909bf978cb8479505f2255d5d238
avillaq <villafuertequispealex@gmail.com>
2024-05-11 17:14:13 -0500

    Obtener el boton con js y asignar un evento

--- Ejercicio-4/script.js
+++ Ejercicio-4/script.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded',init);
 function init(){
     
-
+    let boton = document.querySelector('#boton');    
+    boton.addEventListener('click',function(){
+        let codigo = document.querySelector('#entrda').value;
+       
+    });
 
 
 }

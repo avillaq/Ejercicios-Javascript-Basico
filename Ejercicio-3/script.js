20159b5114d99a5100726b47461ee61309e8ff98
avillaq <villafuertequispealex@gmail.com>
2024-05-11 16:35:55 -0500

    Crea objetos Date del dia de hoy y del dia de Arequipa

--- Ejercicio-3/script.js
+++ Ejercicio-3/script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded',init);
 function init(){
-    
+    let diaArequipa = new Date();
+    diaArequipa.setFullYear(2024, 8, 15);
+
+    let diaHoy = new Date();
 
 
 

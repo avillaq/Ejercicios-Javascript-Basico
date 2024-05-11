813645339fe35df7196443c73e1434d029bf650f
avillaq <villafuertequispealex@gmail.com>
2024-05-11 12:47:07 -0500

    Crea una funcion para obteer una dia de la semana

--- Ejercicio-1/script.js
+++ Ejercicio-1/script.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded',init);
 function init(){
+    function obtenerDia(n){
+        let dias = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
+        return dias[n];
+    }
+
     
-
-
-
 }

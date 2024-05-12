38e2bddf932e24b1099e987e6206b29138285608
avillaq <villafuertequispealex@gmail.com>
2024-05-12 17:31:23 -0500

    Añadir un evento para detectar el cambio del tipo de operacion

--- Ejercicio6/pagina2/script.js
+++ Ejercicio6/pagina2/script.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded',init);
 function init(){
-    
+    let tipoOperacion = document.querySelector('#tipoOperacion');
+
+    tipoOperacion.addEventListener('change',function(){
+        let valor = tipoOperacion.value;
+        console.log(valor);
+
+    });
     
 
 

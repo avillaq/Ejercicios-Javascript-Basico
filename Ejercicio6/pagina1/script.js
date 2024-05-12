2a4fca2993923e890b8708f7d7e649911a369639
avillaq <villafuertequispealex@gmail.com>
2024-05-12 13:15:28 -0500

    Obtner los elementos por su id y añadir un evento al boton aumentar

--- Ejercicio6/pagina1/script.js
+++ Ejercicio6/pagina1/script.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded',init);
 function init(){
-    
+    let textoPrueba = document.querySelector('#textoPrueba');
+    let btnAumentar = document.querySelector('#btnAumentar');
+    let btnReducir = document.querySelector('#btnReducir');
 
-
+    btnAumentar.addEventListener('click',function(){
+        let fontSize = textoPrueba.style.fontSize;
+        fontSize += 5;
+        textoPrueba.style.fontSize = `${fontSize}px`;
+    });
 
 }

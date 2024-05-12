ecd46f860f501c516bb9d5d34c9bc3e727a03de8
avillaq <villafuertequispealex@gmail.com>
2024-05-12 11:37:33 -0500

    Crear un arreglo para determinar las columnas

--- Ejercicio-5/script.js
+++ Ejercicio-5/script.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded',init);
 function init(){
+    let limiteColumnas = [1,4,9,16,25,36,49,64,81,100];
     let form = document.querySelector('#form');
     form.addEventListener('submit',function(event){
         event.preventDefault();
         let numeroValores = document.querySelector('#numeroValores').value;
-        
+        numeroValores = parseInt(numeroValores);
     });
 
 

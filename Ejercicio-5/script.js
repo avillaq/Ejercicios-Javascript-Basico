883cb7ccbcacb2b5daf9847f08fb0ec2bb138f6e
avillaq <villafuertequispealex@gmail.com>
2024-05-11 23:16:00 -0500

    Obtner el formulario y crear un evento submit

---
+++ Ejercicio-5/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',init);
+function init(){
+    let form = document.querySelector('#form');
+    form.addEventListener('submit',function(event){
+        event.preventDefault();
+        
+    });
+
+
+
+}
